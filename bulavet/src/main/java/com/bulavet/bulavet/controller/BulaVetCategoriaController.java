69e9629899d61127e5cd37d378d201a33f3808d9
Raphael Scaziti <rscaziti@gmail.com>
2021-02-28 12:53:57 -0300

    feat(categoria-controller): adicionado método GetById

--- bulavet/src/main/java/com/bulavet/bulavet/controller/BulaVetCategoriaController.java
+++ bulavet/src/main/java/com/bulavet/bulavet/controller/BulaVetCategoriaController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,5 +25,13 @@ public class BulaVetCategoriaController
 	public ResponseEntity<List<BulaVetCategoriaModel>> GetAll()
 	{
 		return ResponseEntity.ok(repository.findAll());	
+	}
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<BulaVetCategoriaModel> GetById(@PathVariable Long id)
+	{
+		return this.repository.findById(id)
+				.map(resp -> ResponseEntity.ok(resp))
+				.orElse(ResponseEntity.notFound().build());
 	}
 }
