da44ffc84dc9ec5f6c8042e08f4ab2e9e79f5bf4
Raphael Scaziti <rscaziti@gmail.com>
2021-02-28 14:42:32 -0300

    feat(farmaco-controller): adicionado método GetByNome

--- bulavet/src/main/java/com/bulavet/bulavet/controller/BulaVetFarmacoController.java
+++ bulavet/src/main/java/com/bulavet/bulavet/controller/BulaVetFarmacoController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -24,5 +25,11 @@ public class BulaVetFarmacoController
 	public ResponseEntity<List<BulaVetFarmacoModel>> GetAll()
 	{
 		return ResponseEntity.ok(this.repository.findAll());
+	}
+	
+	@GetMapping("/{nomeFarmaco}")
+	public ResponseEntity<List<BulaVetFarmacoModel>> GetByNome(@PathVariable String nomeFarmaco)
+	{
+		return ResponseEntity.ok(this.repository.findAllByNomeFarmacoContainingIgnoreCase(nomeFarmaco));
 	}
 }
