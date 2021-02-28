88e5feff5f406d525f3e56d7cb71f7d853c9a4d9
Raphael Scaziti <rscaziti@gmail.com>
2021-02-28 13:16:20 -0300

    feat(categoria-controller): adicionado método PUT

--- bulavet/src/main/java/com/bulavet/bulavet/controller/BulaVetCategoriaController.java
+++ bulavet/src/main/java/com/bulavet/bulavet/controller/BulaVetCategoriaController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -48,5 +49,11 @@ public class BulaVetCategoriaController
 	public ResponseEntity<BulaVetCategoriaModel> post(@RequestBody BulaVetCategoriaModel categoria)
 	{
 		return ResponseEntity.status(HttpStatus.CREATED).body(this.repository.save(categoria));
+	}
+	
+	@PutMapping
+	public ResponseEntity<BulaVetCategoriaModel> put(@RequestBody BulaVetCategoriaModel categoria)
+	{
+		return ResponseEntity.status(HttpStatus.OK).body(this.repository.save(categoria));
 	}
 }
