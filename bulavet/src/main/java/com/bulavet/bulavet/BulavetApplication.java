6f271846dad8b0959c154ec70e62e73e52423f86
Raphael Scaziti <rscaziti@gmail.com>
2021-02-28 12:23:34 -0300

    feat(initializr): adicionado arquivo inicial do spring, com dependencias

---
+++ bulavet/src/main/java/com/bulavet/bulavet/BulavetApplication.java
@@ -0,0 +1,13 @@
+package com.bulavet.bulavet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BulavetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BulavetApplication.class, args);
+	}
+
+}
---
+++ bulavet/src/test/java/com/bulavet/bulavet/BulavetApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bulavet.bulavet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BulavetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
